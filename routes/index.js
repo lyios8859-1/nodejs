4f53b63241e9fa56d94bfd1849b2416b1edd793f
ly <94962218@qq.com>
2017-09-03 16:55:17 +0800

    bug修改

--- routes/index.js
+++ routes/index.js
@@ -2,44 +2,44 @@ var express = require('express');
 var router = express.Router();
 var trimHtml = require('trim-html');
 //console.log(trimHtml+"===");
-
 var Content = require("../dao/content");
-
+//首页查询数据
 router.get('/', function(req, res, next) {
   Content.find({status:1,isdelete:0},function(err,result){ 
     if(!err){
-
-      //数据的进一步处理
-      var newData = result.map(function(document,index,currentArr){
-           var data = {
-              title:document.getTitle(),
-              //content:trimHtml(document.content, { limit: 5 }).html//已经放在自定义标签处理了,这就可以不用了
-              content:document.content
-            };
-           //对象混入
-           return mix(document,data);
-       });
-
-       res.render('index', { result:newData});
+      Content.count({status:1,isdelete:0},function(err,count){
+          if(!err){
+            //数据的进一步处理
+            var newData = result.map(function(document,index,currentArr){
+                 var data = {
+                    title:document.getTitle(),
+                    //content:trimHtml(document.content, { limit: 5 }).html//已经放在自定义标签处理了,这就可以不用了
+                    content:document.content
+                  };
+                  //对象混入
+                  return mix(document,data);
+              });
+            //异步返回查询数据和总数
+            //res.send({result:result,totalCount:count});
+            res.render("index",{result:newData,totalCount:count});
+          }
+      });
     }
-  }).sort({"ctime":"desc"}).skip(0).limit(10);
+  }).sort({"ctime":"desc"}).skip(0).limit(3);
 });
-//分页对象
+/*//分页对象
 var Params = {
   pageNo:0,
   pageSize:3
 };
 
-/*搜索*/
+//搜索并分页
 router.post('/search', function(req, res, next) {
-
   //获取前端ajax提交过来的分页信息
   Params.pageNo = parseInt(req.body.pageNo || 0,10);
   Params.pageSize = parseInt(req.body.pageSize || 3,10);
-
   //查询条件
   var params = {status:1,isdelete:0};
-
   //获取关键词进行检索
   var keyword = req.body.keyword;
   if(keyword){
@@ -49,15 +49,14 @@ router.post('/search', function(req, res, next) {
         {title:{$regex:keyword,$options:"i"}},
         {content:{$regex:keyword,$options:"i"}}
     ];
-    params["$or"] = likeArr;
+    params["$or"] = likeArr; //or查询
   }
 
-  
   //查询所有数据
   Content.find(params,function(err,result){
         if(!err){
           //求总数是不需要分页和排序的
-          Content.count(params,function(err,count){//32
+          Content.count(params,function(err,count){
             if(!err){
               //数据的进一步处理
               var newData = result.map(function(document,index,currentArr){
@@ -75,12 +74,63 @@ router.post('/search', function(req, res, next) {
             }
           });
         }
-  }).sort({ctime:"desc"})
-  .skip(Params.pageNo)
-  .limit(Params.pageSize);
+  }).sort({ctime:"desc"}).skip(Params.pageNo).limit(Params.pageSize);
+});*/
+
+
+
+//分页对象
+var Params = {
+  pageNo:0,
+  pageSize:3
+};
+
+//搜索
+router.post('/search', function(req, res, next) {
+
+  //获取前端ajax提交过来的分页信息
+  Params.pageNo = parseInt(req.body.pageNo||0, 10);
+  Params.pageSize = parseInt(req.body.pageSize||3, 10);
+  //查询条件
+  var params = {status:1,isdelete:0};
+  //获取关键词进行检索
+  var keyword = req.body.keyword;
+  if(keyword){
+    console.log(keyword);
+    //模糊搜索的条件
+    var likeArr = [
+        {title:{$regex:keyword,$options:"i"}},
+        {content:{$regex:keyword,$options:"i"}}
+    ];
+    params["$or"] = likeArr; //or查询
+
+  }
+
+  //查询所有数据
+  Content.find(params,function(err,result){
+        if(!err){
+          //求总数是不需要分页和排序的
+          Content.count(params,function(err,count){
+            if(!err){
+              //数据的进一步处理
+              var newData = result.map(function(document,index,currentArr){
+                   var data = {
+                      title:document.getTitle(),
+                      //content:trimHtml(document.content, { limit: 5 }).html//已经放在自定义标签处理了,这就可以不用了
+                      content:document.content
+                    };
+                   //对象混入
+                   return mix(document,data);
+               });
+              //异步返回查询数据和总数
+              //res.send({result:result,totalCount:count});
+              //console.log("---------"+newData+"---------"+ count+"---------------"+keyword);
+              res.render("partials/contenttemplate",{result:newData,totalCount:count,layout:null});
+            }
+          });
+        }
+  }).sort({ctime:"desc"}).skip(Params.pageNo).limit(Params.pageSize);
 });
-
-
 
 //详情页面
 router.get('/pages/:id', function(req, res, next) {
@@ -91,13 +141,10 @@ router.get('/pages/:id', function(req, res, next) {
     });
 });
 
-
 //添加页面
 router.get('/add', function(req, res, next) {
     res.render("add");
 });
-
-
 
 //保存的业务
 router.post('/save', function(req, res, next) {
@@ -109,8 +156,6 @@ router.post('/save', function(req, res, next) {
       }
   });
 });
-
-
 
 //详情页面
 router.get('/edit/:id', function(req, res, next) {
@@ -132,18 +177,16 @@ router.post('/update/:id', function(req, res, next) {
     });
 });
 
-
 //删除页面
 router.post('/del', function(req, res, next) {
-  Content.remove({_id:req.body.opid},function (err) {
-    if (!err){
-      res.send("success")
-    }else{
-      res.send("fail");
-    }
-  })
+ Content.remove({_id:req.body.opid},function (err) {
+   if (!err){
+      res.send("success");
+   }else{
+     res.send("fail");
+   }
+ })
 });
-
 
 //多对象混入
 function mix(target,source){
@@ -164,7 +207,5 @@ function mix(target,source){
 	}
 	return target;
 };
-
-
 
 module.exports = router;
