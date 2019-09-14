52a6bf18850409e0c4e86d632f8393083e49d366
lyios88598-1 <194962218@qq.com>
2019-09-14 14:51:16 +0800

    :bug:

--- core/Connection.js
+++ core/Connection.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
-// try {
+
   mongoose.connect('mongodb://localhost:27017/myblog', {useUnifiedTopology: true, useNewUrlParser: true});
+
   //创建一个连接对象
   const db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'connection error:'));
+  
+  db.on('error', function(error) {
+    mongoose.close();
+    console.error('connection error:', error);
+  });
+
   db.once('open', function (callback) {
     console.log("数据库连接成功....");
   });
-// } catch (error) {
-//   mongoose.close();
-//   console.error("数据库连接失败...", error);
-// }
-
-
 module.exports = mongoose;
