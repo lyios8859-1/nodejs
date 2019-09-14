636d55b43eb44167b0bc227407fd2f565dcb6e8c
lyios88598-1 <194962218@qq.com>
2019-09-14 13:54:57 +0800

    login

--- core/Connection.js
+++ core/Connection.js
@@ -4,7 +4,6 @@ try {
   //创建一个连接对象
   const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
-  
   db.once('open', function (callback) {
     console.log("数据库连接成功....");
   });
--- dao/user.js
+++ dao/user.js
@@ -3,15 +3,14 @@ var mongoose = require("../core/Connection");
 
 // 建立一个Schema的文档对象
 var UserSchema = mongoose.Schema({
-    username: String,// 标题
-    ctime: { type: Date, default: Date.now },// 创建时间
-    account:String,// 内容
-    password:String,// 外面连接
-    utime:{type:Date ,default:null},// 更新时间
-    status:{ type: Number, default: 1},/* 0未发布1发布 */
-    isdelete:{ type: Number, default: 0}/* 0未删除1删除,逻辑删除：只是改一个状态，数据还是保留，物理删除：数据库不存在*/
+    username: String,// 用户名
+    // account: String,// 昵称
+    password: String,// 密码
+    ctime: { type: Date, default: Date.now } // 创建时间
 });
 
 // 创建集合(创建表)
-var User = mongoose.model('User', ContentSchema);
+var User = mongoose.model('User', UserSchema);
 
+
+module.exports = User;
