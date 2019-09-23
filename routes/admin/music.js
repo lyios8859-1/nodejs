178f08b95150bafe666a58866311c295b199808d
lyios88598-1 <194962218@qq.com>
2019-09-24 00:11:18 +0800

    :sparkle:

--- routes/admin/music.js
+++ routes/admin/music.js
@@ -3,6 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.send('musin page');
+  res.render('admin/music/index', { title: '歌曲欣赏', layout: null });
 });
 module.exports = router;
